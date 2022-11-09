0d8335a246132d87ebac643dec4e4cd63551e762
Brian Murphy <ktp200@gmail.com>
2022-11-09 18:06:46 -0500

    added multiple character types, shuffles in guaranteed character types starting from the middle

--- script.js
+++ script.js
@@ -1,6 +1,107 @@
-// array of numeric digits
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+//ARRAYS
 
+// array of lowercase characters
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// array of uppercase charaacters
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+
+// array of numeric digits
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// array of special characters, list from owasp.org
+var specialCharacters = [
+  " ",
+  "!",
+  '"',
+  "#", 
+  "$",
+  '%',
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+
+//FUNCTIONS
 
 // function for getting a random element from an array
 function getRandom(arr) {
@@ -8,7 +109,6 @@ function getRandom(arr) {
   //console.log(randElement);
   return randElement;
 }
-
 
 //input options function
 function inputOptions() {
@@ -18,33 +118,57 @@ function inputOptions() {
 
   // user input length
   var length = parseInt(
-    prompt('How many characters long should the password be? Input a number between 8 and 128.')
+    prompt("How many characters long should the password be? Input a number between 8 and 128.")
   );
 
   // check length of the password, quit the function if it's false
   if (length < 8) {
-    alert('Please enter a number between 8 and 128.');
+    alert("Please enter a number between 8 and 128.");
     return null;
   }
 
   if (length > 128) {
-    alert('Please enter a number between 8 and 128.');
+    alert("Please enter a number between 8 and 128.");
     return null;
   }
 
   //if it's not a number, quit the function
   if (Number.isNaN(length)) {
-    alert('That is not a number!');
+    alert("That is not a number!");
     return null;
   }
 
-  var  lowercaseOK
+  // Variable to store boolean regarding the inclusion of special characters
+  var lowercaseOK = confirm(
+    "Click OK to include lowercase characters."
+  );
 
-  var  uppercaseOK
+  // Variable to store boolean regarding the inclusion of numeric characters
+  var uppercaseOK = confirm(
+    "Click OK to include uppercase characters."
+  );
 
-  var  numbersOK
+  // Variable to store boolean regarding the inclusion of lowercase characters
+  var numbersOK = confirm(
+    "Click OK to include numeric characters."
+  );
 
-  var  specialOK
+  // Variable to store boolean regarding the inclusion of uppercase characters
+  var specialOK = confirm(
+    "Click OK to include special characters."
+  );  
+ 
+
+  //exit function if no types were selected
+  if (
+    lowercaseOK == false &&
+    uppercaseOK == false &&
+    numbersOK == false &&
+    specialOK == false
+  ) {
+    alert("You must OK at least one type of character.");
+    return null;
+  }
 
   // user selected options
   var options = {
@@ -68,15 +192,57 @@ function generatePassword() {
   // Get the options
   var options = inputOptions();
 
-  // If options returned null, exit the function
+  // This array will contain all elements from arrays that were OK'd
+  var possibleCharacters = [];
+
+  // This array contains one element from each OK'd array, to ensure at least one of each type is in the password
+  var guaranteedCharacters = [];
+
+  // If options returned nothing, exit the function
   if (!options) return null;
 
-  //adds a random number until the length of the password is long enough
-  for (var i = 0; i < options.length; i++) {
-    var numericAdd = getRandom(numericCharacters)
-    passwordOutput.push(numericAdd);
+
+  //if lowercase is OK, add it to the list of characters to use in the password
+  if (options.lowercaseOK) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    //add one random lowercase character to the guaranteed list
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
   }
 
+ //if uppercase is OK, add it to the list of characters to use in the password
+  if (options.uppercaseOK) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    //add one random uppercase character to the guaranteed list
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+  }
+
+
+ //if numbers are OK, add them to the list of characters to use in the password
+  if (options.numbersOK) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    //add one random number to the guaranteed list
+    guaranteedCharacters.push(getRandom(numericCharacters));
+  }
+
+  //if special characters are OK, add them to the list of characters to use in the password
+  if (options.specialOK) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    //add one random special character to the guaranteed list
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+  //adds a random character until the length of the password is long enough
+  for (var i = 0; i < options.length; i++) {
+    passwordOutput.push(getRandom(possibleCharacters));
+  }
+
+  // put in some of the guaranteed characters
+  for (var x = Math.floor(options.length / 2), i = 0; i < guaranteedCharacters.length; i++, x++) {
+    //console.log(guaranteedcharacters[i]);
+    passwordOutput[x] = guaranteedCharacters[i];
+  }
+
+  //bring all the characters together into one string
   return passwordOutput.join("");
 }
 
